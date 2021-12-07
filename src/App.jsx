11fe9291b165bd4880517cb28e1bab374df5bc24
Alysson <alysson6801@hotmail.com>
2021-12-07 11:29:07 -0300

    chore: using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { Counter } from './Components/Counter'
 import { RepositoryList } from './Components/RespositoryList'
 import './styles/global.scss'
 
-export function App(){
-  return <RepositoryList />
+export function App() {
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  )
 }
